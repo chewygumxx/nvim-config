fdc4ae0e89976ccead37f2846c0d60b6efd0af1b
chewygumxx <cgumxx@pm.me>
2026-09-15 10:00:46 +1000

    style(nvim/systemd): Filetype systemd -> dosini

--- lua/filetype/ftmatrix.lua
+++ lua/filetype/ftmatrix.lua
@@ -13,26 +13,44 @@
 -- Heuristic filetype resolution.
 --
 
-local M = {}
+local M = {
+    extension = {
+    },
+    filename  = {
+        ["ignore"]         = "gitignore",
+        [".chezmoiignore"] = "gitignore",
+        [".assetsignore"]  = "gitignore",  -- CloudFlare Worker wrangler config
+    },
+    pattern   = {
+        [".*gnupg/.*%.conf"] = "gpg",
+        [".*/hypr/.*%.conf"] = "hyprlang",
+
+        [".*config/zsh/.*"]       = "zsh",
+        [".*zsh/func/[^/]*"]      = "zsh",
+        [".*zsh/functions/[^/]*"] = "zsh",
+    },
+}
+
+local define_dosini = function()
+    local exts = {
+        "conf",
+
+        -- Systemd
+        "automount", "device", "mount", "path", "scope", "service",
+        "slice", "snapshot", "socket", "swap", "target", "timer",
+    }
+
+    for _,ext in ipairs(exts) do
+        M.extension[ext] = "dosini"
+    end
+end
 
 M.setup = function()
+    define_dosini()
     vim.filetype.add({
-        extension = {
-            zshrc = "zsh",
-        },
-        filename = {
-            ["ignore"]         = "gitignore",
-            [".chezmoiignore"] = "gitignore",
-            [".assetsignore"]  = "gitignore",  -- CloudFlare Worker wrangler config
-        },
-        pattern = {
-            [".*gnupg/.*%.conf"] = "gpg",
-            [".*/hypr/.*%.conf"] = "hyprlang",
-
-            [".*config/zsh/.*"]       = "zsh",
-            [".*zsh/func/[^/]*"]      = "zsh",
-            [".*zsh/functions/[^/]*"] = "zsh",
-        },
+        extension = M.extension,
+        filename  = M.filename,
+        pattern   = M.pattern,
     })
 end
 
