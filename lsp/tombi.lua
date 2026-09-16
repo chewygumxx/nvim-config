486d716159cfae208fea9186f8fe5c9da6c30fd6
chewygumxx <cgumxx@pm.me>
2026-09-17 08:02:00 +1000

    feat(nvim): Highlight overrides lsp/tombi

--- lsp/tombi.lua
+++ lsp/tombi.lua
@@ -12,4 +12,19 @@
 ---@type vim.lsp.Config
 local M = {}
 
+local hlgroup_defs = {
+    ["@lsp.type.key.toml"] = { link = "@property" },
+    ["@lsp.type.table.toml"] = { link = "@property" },
+}
+
+local highlight = function()
+    for hlgroup, defmap in pairs(hlgroup_defs) do
+        vim.api.nvim_set_hl(0, hlgroup, defmap)
+    end
+end
+
+M.on_attach = function()
+    highlight()
+end
+
 return M
