ec0c4fded54ae7afc966f40e7ca5808c616d19f5
chewygumxx <cgumxx@pm.me>
2026-09-21 06:30:12 +1000

    feat(spec): Add snacks.nvim

---
+++ lua/spec/snacks.nvim.lua
@@ -0,0 +1,37 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: GPL-3.0-only
+
+--
+--
+-- ~chewygumxx/nvim-config.git
+-- ::: :/lua/spec/snacks.nvim.lua
+--
+--
+
+local M = {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        explorer = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+    },
+}
+
+return M
