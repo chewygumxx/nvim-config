7239c091cf6fda5d83f07e253511e85559c8d062
chewygumxx <cgumxx@pm.me>
2026-09-21 07:16:33 +1000

    feat: Route quickfix/loclist through Telescope

--- lua/spec/telescope.nvim.lua
+++ lua/spec/telescope.nvim.lua
@@ -18,6 +18,11 @@
 local M = {
     "nvim-telescope/telescope.nvim",
 
+    -- Not lazy: the quickfix/loclist redirect below (`config`, not `opts`)
+    -- must be registered before anything ever opens one, which can happen
+    -- before Telescope would otherwise have been triggered to load.
+    lazy = false,
+
     opts = {},
 
     dependencies = {
@@ -25,5 +30,30 @@ local M = {
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
 }
+
+M.config = function(_, opts)
+    require("telescope").setup(opts)
+
+    -- Route quickfix/location-list windows through Telescope's own picker
+    -- instead of Neovim's native quickfix UI. `FileType qf` fires for both;
+    -- `getwininfo().loclist` tells them apart.
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern  = "qf",
+        callback = function()
+            local win = vim.api.nvim_get_current_win()
+            local loc = vim.fn.getwininfo(win)[1].loclist == 1
+            vim.schedule(function()
+                local builtin = require("telescope.builtin")
+                if loc then
+                    vim.cmd("lclose")
+                    builtin.loclist()
+                else
+                    vim.cmd("cclose")
+                    builtin.quickfix()
+                end
+            end)
+        end,
+    })
+end
 
 return M
