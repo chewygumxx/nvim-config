9127a6aee9844fc11fd904fca638e288086f79bc
chewygumxx <cgumxx@pm.me>
2026-09-21 06:20:49 +1000

    feat(spec): Add gist.nvim

---
+++ lua/spec/gist.nvim.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: GPL-3.0-only
+
+--
+--
+-- ~chewygumxx/nvim-config.git
+-- ::: :/lua/spec/gist.nvim.lua
+--
+--
+
+local M = {
+    "Rawnly/gist.nvim",
+    config = true,
+    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+}
+
+return M
