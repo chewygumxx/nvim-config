c5fc454524dae5b51dbe8c4cf42edf2a0b97c646
chewygumxx <cgumxx@pm.me>
2026-09-21 13:59:55 +1000

    fix(spec): Populate nvim-notify

--- lua/spec/nvim-notify.lua
+++ lua/spec/nvim-notify.lua
@@ -10,13 +10,37 @@
 --
 
 ---@module "lazy"
+---@module "notify"
 
 ---@type LazyPluginSpec
 local M = {
     "rcarriga/nvim-notify",
     lazy = false,
 
-    opts = {},
+    -- The README is outdated, use `:h notify.Config`
+    ---@type notify.Config
+    opts = {
+        merge_duplicates = true,
+        background_colour = "NotifyBackground",
+        fps = 30,
+        icons = {
+            DEBUG = "",
+            ERROR = "",
+            INFO = "",
+            TRACE = "✎",
+            WARN = "",
+        },
+        level = 2,
+        minimum_width = 50,
+        render = "default",
+        stages = "fade_in_slide_out",
+        time_formats = {
+            notification = "%T",
+            notification_history = "%FT%T",
+        },
+        timeout = 5000,
+        top_down = true,
+    },
 }
 
 return M
