6ba91e7bec7136e697e09cb988fada5407ff1bce
chewygumxx <cgumxx@pm.me>
2026-09-21 11:09:39 +1000

    feat(spec): Add markview.nvim

---
+++ lua/spec/markview.nvim.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: GPL-3.0-only
+
+--
+--
+-- ~chewygumxx/nvim-config.git
+-- ::: :/lua/spec/markview.nvim.lua
+--
+--
+
+---@module "lazy"
+
+---@type LazyConfig
+local M = {
+    "OXY2DEV/markview.nvim",
+
+    -- The README advises against lazy-loading citing preview loading delay.
+    -- I can wait.
+    lazy = true,
+    ft = "markdown",
+
+    dependencies = { "saghen/blink.cmp" },
+    opts = {},
+}
+
+return M
