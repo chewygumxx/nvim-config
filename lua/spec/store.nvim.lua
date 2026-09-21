ba233eae1c4213d77d6d4894f2be4f9121e1538e
chewygumxx <cgumxx@pm.me>
2026-09-21 11:10:02 +1000

    feat(spec): Add store.nvim

---
+++ lua/spec/store.nvim.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: GPL-3.0-only
+
+--
+--
+-- ~chewygumxx/nvim-config.git
+-- ::: :/lua/spec/store.nvim.lua
+--
+--
+
+---@module "lazy"
+
+---@type LazyPluginSpec
+local M = {
+    "alex-popov-tech/store.nvim",
+    lazy = true,
+    cmd = "Store",
+
+    dependencies = {
+        { "OXY2DEV/markview.nvim", opts = {} },
+        { "3rd/image.nvim", opts = {} },
+    },
+
+    opts = {
+        layout = "tab", -- recommended when using image preview
+    },
+}
+
+return M
