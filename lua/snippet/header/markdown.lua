0cd2580c52e80b9ef03063636eda32590e219ba9
chewygumxx <cgumxx@pm.me>
2026-09-21 13:43:25 +1000

    chore: Drop stale luasnip diagnostic suppression

--- lua/snippet/header/markdown.lua
+++ lua/snippet/header/markdown.lua
@@ -13,13 +13,11 @@
 -- LuaSnip snippet for markdown header
 --
 
-local ls = require("luasnip")
----@diagnostic disable: undefined-field -- luasnip.lua is disabled, type stub unavailable
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
----@diagnostic enable: undefined-field
+local ls  = require("luasnip")
+local s   = ls.snippet
+local t   = ls.text_node
+local i   = ls.insert_node
+local f   = ls.function_node
 local rep = require("luasnip.extras").rep
 
 local git  = require("util.git")
