f1820783b355c466e7576bc59de5ad1a3fe75dde
chewygumxx <cgumxx@pm.me>
2026-09-21 18:37:43 +1000

    refactor(spec): Disabling overrides

--- init.lua
+++ init.lua
@@ -82,18 +82,25 @@ _G.setup_guard("util.lazy", {
         {
             name = "overrides",
             import = function()
-                return {
-                    { cond = false, "folke/noice.nvim" },
-                    { cond = false, "folke/snacks.nvim" },
-                    { cond = false, "L3M0NAD3/LuaSnip" },
-                    { cond = false, "mikavilpas/yazi.nvim" },
-                    { cond = false, "stevearc/oil.nvim" },
-                    { cond = false, "kndndrj/nvim-dbee" },
-                    { cond = false, "MunifTanjim/nvim-nio" },
-                    { cond = false, "OXY2DEV/markview.nvim" },
-                    { cond = false, "folke/which-key.nvim" },
-                    { cond = false, "nvim-neorg/neorg" },
+                local elide = {
+                    "folke/noice.nvim",
+                    "folke/snacks.nvim",
+                    "L3M0NAD3/LuaSnip",
+                    "mikavilpas/yazi.nvim",
+                    "stevearc/oil.nvim",
+                    "kndndrj/nvim-dbee",
+                    "MunifTanjim/nvim-nio",
+                    "OXY2DEV/markview.nvim",
+                    "folke/which-key.nvim",
+                    "nvim-neorg/neorg",
+                    "rcarriga/nvim-notify",
+                    "nvim-telescope/telescope.nvim",
                 }
+                local ret   = {}
+                for _, plugin in ipairs(elide) do
+                    table.insert(ret, { cond = false, plugin })
+                end
+                return ret
             end,
         },
     },
