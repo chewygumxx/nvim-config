2de4343e5d15bdb7d6c7a0edbbd3fe5f201d1dc2
chewygumxx <cgumxx@pm.me>
2026-09-24 06:43:48 +1000

    fix(ucmd): Remove dead require-nil guards

--- lua/usercmd/init.lua
+++ lua/usercmd/init.lua
@@ -21,11 +21,7 @@ local usercmd = vim.api.nvim_create_user_command
 ---@return nil
 local visual_traversal = function()
     local desc = "Visual traversal in current buffer"
-    local mod  = require("util.visual_traversal")
-    if not mod then
-        return
-    end
-    local vt = mod.command
+    local vt   = require("util.visual_traversal").command
     usercmd("XXVisTrav", assert(vt("toggle")), { desc = "Toggle: " .. desc })
     usercmd("XXVisTravToggle", assert(vt("toggle")), {
         desc = "Toggle: " .. desc,
@@ -43,9 +39,6 @@ end
 local interpret_escape = function()
     local desc = "Translate and interpret escape codes in terminal buffer"
     local ie   = require("usercmd.interpret_escape")
-    if not ie then
-        return
-    end
     usercmd("XXInterpretEscape", ie.command, { desc = desc, bang = true })
 end
 
@@ -54,11 +47,7 @@ end
 local redirect_awkward_pager = function()
     local desc    = "Redirect to temporary buffer (bypass bang!): "
     local vimcmds = { "autocmd", "command", "highlight", "map" }
-    local mod     = require("usercmd.redirect_awkward_pager")
-    if not mod then
-        return
-    end
-    local rap = mod.command
+    local rap     = require("usercmd.redirect_awkward_pager").command
     for _, vimcmd in ipairs(vimcmds) do
         local capitalvcmd, _ = vimcmd:gsub("^%l", string.upper)
         usercmd("XXRedir" .. capitalvcmd, rap(vimcmd), {
@@ -82,9 +71,6 @@ local insert_header = function()
     local desc = "Prepend buffer with a header, "
         .. "templated according to filepath and extension."
     local ih   = require("util.header")
-    if not ih then
-        return
-    end
     usercmd("XXInsertHeader", ih.command, { desc = desc })
 end
 
@@ -95,9 +81,6 @@ local lua_checker = function()
     local desc = "Switch nvim-lint's lua type checker "
         .. "(luals_check/emmylua_check); bare toggles"
     local mod  = require("usercmd.lua_checker")
-    if not mod then
-        return
-    end
     usercmd("XXLuaChecker", mod.command, {
         desc = desc,
         nargs = "?",
